3c5f903d4afca1fdfffd8f35911046b58d8e6c05
Ilyailyushin <ilyusha23ilyushin1@gmail.com>
2024-08-18 14:10:04 +0300

    Add files via upload

---
+++ load.js
@@ -0,0 +1,34 @@
+const startBtn = document.querySelector(".start-btn");
+const wrap = document.querySelector(".wrap");
+
+let parallelTaskNum = 1;
+let data;
+
+fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+  .then((res) => res.json())
+  .then((res) => {
+    data = res;
+  })
+  .catch((err) => console.log(err.message));
+
+startBtn.addEventListener("click", startLoadingImages);
+
+function startLoadingImages() {
+  let i = 0;
+  const chunkSize = 50;
+
+  function insertImagesChunk() {
+    let end = Math.min(i + chunkSize, data.length);
+    do {
+      let elem = `<img src=${data[i].url}></img>`;
+      wrap.innerHTML += elem;
+      i++;
+    } while (i < end);
+
+    if (i < data.length) {
+      setTimeout(insertImagesChunk, 0);
+    }
+  }
+
+  insertImagesChunk();
+}
